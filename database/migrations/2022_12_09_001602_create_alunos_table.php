767d05e4e36658b8f3f20b3ad8ca0ea4832194ed
dearlaro <larissa_iara23@hotmail.com>
2022-12-08 22:09:19 -0300

    Iniciando Laravel - CRUD

---
+++ database/migrations/2022_12_09_001602_create_alunos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->id;
+            $table->interger('ra');
+            $table->String('nome');
+            $table->date('dtNascto');
+            $table->integer('telefone');
+            $table->String('responsavel');
+            $table->String('turma');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alunos');
+    }
+};
